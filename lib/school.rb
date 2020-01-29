3a48b489cc7324c8df469685b5a308d35c4f773a
Whitney Kidd <whitneymkidd@gmail.com>
2020-01-29 13:18:02 -0700

    Add initialize @name, @start_time, @hours

---
+++ lib/school.rb
@@ -0,0 +1,22 @@
+class School
+  attr_reader :name,
+
+  def initialize (name_parameter, start_time_parameter, hours_parameter)
+    @name = name_parameter
+    @start_time = start
+    @hours = hours_parameter
+  end
+
+
+  # class Student
+  #   attr_reader :name, :cookies,
+  #   def initialize(name, cookies)
+  #     @name = name
+  #     @cookies = []
+  #   end
+  #
+  #   def add_cookie(cookie)
+  #     @cookies << cookie
+  #   end
+
+  end
